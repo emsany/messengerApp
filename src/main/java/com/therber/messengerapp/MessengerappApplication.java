9e0d8d2c0c56ca2c27c074f9d5810fcee127dc84
Emmanuel SANNI <emsany@gmail.com>
2022-06-08 18:38:27 +0100

    Initialisation

---
+++ src/main/java/com/therber/messengerapp/MessengerappApplication.java
@@ -0,0 +1,13 @@
+package com.therber.messengerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessengerappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/therber/messengerapp/MessengerappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.therber.messengerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
